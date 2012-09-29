0b450eb568da58f735ec3f3cd7865165a9fefd0e
Diego Martinez <mad@xeuz-reloaded.(none)>
2012-09-29 01:49:31 -0500

    template c++

---
+++ src/template.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+
+using namespace std;
+
+int main(){
+
+  return 0;
+}
