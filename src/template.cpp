dca649145122a80273314208889568f113b7d9d3
Diego Martinez <mad@xeuz-reloaded.(none)>
2012-09-29 01:55:28 -0500

    template c++

--- src/template.cpp
+++ src/template.cpp
@@ -10,6 +10,15 @@
 
 using namespace std;
 
+#define D(x) cout << #x " = " << x << endl;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define REP(i,n) rep(i,0,n)
+
+typedef long long int lld;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+
 int main(){
 
   return 0;
